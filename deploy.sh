eb5e32f639abbe37095abe2ff39034df65c09943
arashghafoory <arash.ghafoory@gmail.com>
2025-05-25 12:28:49 +0330

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -e
 
-IMAGE_TAG="#{BuildVariables.IMAGE_TAG}"
-AWS_ACCOUNT_ID="#{BuildVariables.AWS_ACCOUNT_ID}"
-AWS_REGION="#{BuildVariables.AWS_REGION}"
-APP_NAME="#{BuildVariables.APP_NAME}"
+APP_NAME="todo-frontend"
+AWS_ACCOUNT_ID="706712229921"
+AWS_REGION="us-east-1"
+IMAGE_TAG=$(cat /home/ec2-user/app/imageTag.env | cut -d '=' -f2)
 
-IMAGE_URI="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$APP_NAME:$IMAGE_TAG"
+IMAGE_URI="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/todoapp/frontend:$IMAGE_TAG"
 
 echo "Logging in to ECR..."
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 echo "Pulling image: $IMAGE_URI"
 docker pull $IMAGE_URI
+
+echo "Stopping old container..."
 docker stop $APP_NAME || true
 docker rm $APP_NAME || true
+
+echo "Running new container..."
 docker run -d --name $APP_NAME -p 80:80 $IMAGE_URI
